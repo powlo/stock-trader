2ca133b23ca7a27ae0165a8530a2836d6cf2ca96
Paul Backhouse <paulcbackhouse@gmail.com>
2018-05-28 16:50:31 +0000

    Move vue.use stuff into main.js

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,9 @@ import Vue from 'vue';
 
 import NavBar from './components/NavBar.vue';
 import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
 
 import routes from './routes';
 import store from './store';
-
-Vue.use(VueRouter);
-Vue.use(VueResource);
 
 const router = new VueRouter({
   routes: routes
